b5fb95ed0380cc9808a233591ffe07355474bfdb
Coby West <4885716+cobyw@users.noreply.github.com>
2024-02-08 15:32:11 -0600

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JobTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void jobTitle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCompoundTitle();
+        }
+
+        private void companyName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCompoundTitle();
+        }
+
+        private void UpdateCompoundTitle()
+        {
+            if (companyName != null && jobTitle != null)
+            {
+                compoundTitle.Content = string.Format("{0} - {1}", companyName.Text, jobTitle.Text);
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not yet implemented.");
+        }
+    }
+}

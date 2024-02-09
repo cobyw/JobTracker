36b6839f5df4d8c3c0efeb58d1c91cd3e9fcb2bf
cobyw <4885716+cobyw@users.noreply.github.com>
2024-02-09 13:35:31 -0600

    Moved tools into their own script

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -145,56 +145,6 @@ namespace JobTracker
             //TODO - Add pop up if they haven't saved recently
         }
 
-
-        /// <summary>
-        /// Writes the given object instance to an XML file.
-        /// <para>Only Public properties and variables will be written to the file. These can be any type though, even other classes.</para>
-        /// <para>If there are public properties/variables that you do not want written to the file, decorate them with the [XmlIgnore] attribute.</para>
-        /// <para>Object type must have a parameterless constructor.</para>
-        /// </summary>
-        /// <typeparam name="T">The type of object being written to the file.</typeparam>
-        /// <param name="filePath">The file path to write the object instance to.</param>
-        /// <param name="objectToWrite">The object instance to write to the file.</param>
-        /// <param name="append">If false the file will be overwritten if it already exists. If true the contents will be appended to the file.</param>
-        public static void WriteToXmlFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
-        {
-            TextWriter writer = null;
-            try
-            {
-                var serializer = new XmlSerializer(typeof(T));
-                writer = new StreamWriter(filePath, append);
-                serializer.Serialize(writer, objectToWrite);
-            }
-            finally
-            {
-                if (writer != null)
-                    writer.Close();
-            }
-        }
-
-        /// <summary>
-        /// Reads an object instance from an XML file.
-        /// <para>Object type must have a parameterless constructor.</para>
-        /// </summary>
-        /// <typeparam name="T">The type of object to read from the file.</typeparam>
-        /// <param name="filePath">The file path to read the object instance from.</param>
-        /// <returns>Returns a new instance of the object read from the XML file.</returns>
-        public static T ReadFromXmlFile<T>(string filePath) where T : new()
-        {
-            TextReader reader = null;
-            try
-            {
-                var serializer = new XmlSerializer(typeof(T));
-                reader = new StreamReader(filePath);
-                return (T)serializer.Deserialize(reader);
-            }
-            finally
-            {
-                if (reader != null)
-                    reader.Close();
-            }
-        }
-
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
             StoreJobData();
@@ -213,7 +163,7 @@ namespace JobTracker
 
             if (dialog.ShowDialog() == true)
             {
-                WriteToXmlFile<List<JobInfo>>(dialog.FileName, jobs);
+                Tools.WriteToXmlFile<List<JobInfo>>(dialog.FileName, jobs);
             }
         }
 
@@ -224,7 +174,7 @@ namespace JobTracker
             {
                 currentSelectionIndex = 0;
                 jobs.Clear();
-                jobs = ReadFromXmlFile<List<JobInfo>>(openFileDialog.FileName);
+                jobs = Tools.ReadFromXmlFile<List<JobInfo>>(openFileDialog.FileName);
                 jobList.Items.Clear();
 
                 foreach (JobInfo info in jobs)
